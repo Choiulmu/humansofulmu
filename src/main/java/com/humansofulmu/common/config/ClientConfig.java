951bd7b95efde5ec96c47f06a544de83432bfdb7
Yurim Choi <azm119@naver.com>
2025-06-02 21:56:38 +0900

    ssmClient 추가

---
+++ src/main/java/com/humansofulmu/common/config/ClientConfig.java
@@ -0,0 +1,17 @@
+package com.humansofulmu.common.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.ssm.SsmClient;
+
+@Configuration
+public class ClientConfig {
+
+    @Bean(name="ssmClient")
+    public SsmClient ssmClient() {
+        return SsmClient.builder()
+                .region(Region.AP_NORTHEAST_2)
+                .build();
+    }
+}
