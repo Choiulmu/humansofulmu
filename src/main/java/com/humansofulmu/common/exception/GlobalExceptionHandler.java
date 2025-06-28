adea4d18d72a503f018d43a83464d1a7f2994611
Yurim Choi <azm119@naver.com>
2025-06-28 21:12:42 +0900

    swagger 이슈로 인한 주석처리

--- src/main/java/com/humansofulmu/common/exception/GlobalExceptionHandler.java
+++ src/main/java/com/humansofulmu/common/exception/GlobalExceptionHandler.java
@@ -1,17 +1,20 @@
 package com.humansofulmu.common.exception;
 
+import com.humansofulmu.PostController;
 import com.humansofulmu.common.resonse.ErrorCode;
 import com.humansofulmu.common.resonse.ErrorResultVO;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import java.util.NoSuchElementException;
 
+//TODO: RestControllerAdvice 재설정
 @Slf4j
-@RestControllerAdvice
+//@RestControllerAdvice(annotations = {RestController.class}, basePackageClasses = {PostController.class})
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(NoSuchElementException.class)
